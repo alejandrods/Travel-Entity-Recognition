89efb0475107890600dff167c785b42c68edfb0c
alejandrods <alejandro.diaz.santos@gmail.com>
2020-05-02 21:28:39 +1000

    Add comments

--- static/main.js
+++ static/main.js
@@ -1,37 +1,31 @@
-
+// Send request when spacebar
 function onKeyDown(e) {
-    if (e.keyCode === 32) { // tab key
+    if (e.keyCode === 32) {
         getNextWordFromService()
     }
 
 }
 
+// Function to send request to model
 function getNextWordFromService() {
-
-    // get model type
     var input = document.getElementById("sentence").value.trim();
     console.log(input)
 
+    // Build data json
     var data = JSON.stringify({"input": input});
     var xhr = new XMLHttpRequest();
     xhr.withCredentials = true;
 
-    console.log("DATA")
-    console.log(data)
-
     xhr.addEventListener("readystatechange", function () {
     if (this.readyState === 4) {
       var data = JSON.parse(this.responseText)
-//      document.getElementById("sentence").value = input + data.next_word;
       document.getElementById("pred").innerHTML = data.pred;
-
-    }
+        }
     });
 
-    // make prediction
+    // Request prediction
     xhr.open("POST", "http://localhost:7000/predict");
     xhr.setRequestHeader("Content-Type", "application/json");
-
     xhr.send(data);
 }
 
